baf6d4b567fe48c1ba88131ff6783841bd5dd565
GMorse19 <malorse@comcast.net>
2019-12-12 23:14:03 -0500

    Initial Commit 2

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://salty-cliffs-30565.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
